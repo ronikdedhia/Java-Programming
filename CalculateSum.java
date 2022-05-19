bb54314b44b2765beab938fc4be41c14a973a68c
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-05-20 00:03:11 +0530

    program to find the sum of positive numbers

---
+++ CalculateSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+// program to find the sum of positive numbers
+class Main {
+  public static void main(String[] args) {
+      
+    int sum = 0;
+    int number = 0;
+    Scanner input = new Scanner(System.in);
+    do {
+      sum += number;
+      System.out.println("Enter a number");
+      number = input.nextInt();
+    } while(number >= 0); 
+	   
+    System.out.println("Sum = " + sum);
+    input.close();
+  }
+}
