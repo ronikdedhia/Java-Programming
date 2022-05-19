d72d2e9132e9bafed87b7f60ecb360d4ddfc7816
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-05-20 00:04:10 +0530

    understand various arithmetic operators

---
+++ ArithmeticOperators.java
@@ -0,0 +1,11 @@
+class ArithmeticOperators {
+  // Arithmetic operators are used to perform arithmetic operations on variables and data
+  public static void main(String[] args) {
+    int a = 12, b = 5;
+    System.out.println("a + b = " + (a + b));
+    System.out.println("a - b = " + (a - b));
+    System.out.println("a * b = " + (a * b));
+    System.out.println("a / b = " + (a / b));
+    System.out.println("a % b = " + (a % b));
+  }
+}
