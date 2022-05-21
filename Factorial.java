6cdf31521887a32b85ee0cf30f90da34952d8a35
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-05-22 01:19:33 +0530

    calculcate factorial using recursion

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+class Factorial {
+    // Factorial of a Number Using Recursion
+    static int factorial( int n ) {
+        if (n != 0) 
+            return n * factorial(n-1);
+        else
+            return 1;
+    }
+
+    public static void main(String[] args) {
+        int number = 4, result;
+        result = factorial(number);
+        System.out.println(number + " factorial = " + result);
+    }
+}
